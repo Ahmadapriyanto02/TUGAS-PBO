6caf09be156267d54b0a2241128ce11d9623f3a7
Ahmadapriyanto02 <83774060+Ahmadapriyanto02@users.noreply.github.com>
2021-05-06 22:17:22 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tugas Program Bilangan Desimal Ke Hexadesimal Visual C#");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Nama      : Ahmad Apriyanto");
+            Console.WriteLine("Npm       : 1101201089");
+            Console.WriteLine("Kelas     : Malam");
+            Console.WriteLine("Dosen     : Asep Mabrur Aid,S.T,M.Kom");
+            Console.WriteLine("Prodi     : Teknik Informatika");
+            Console.WriteLine("MK        : Pemgraman Berorientasi Objek");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            string hexval = "10B0";
+            Console.WriteLine("Hexadecimal number: " + hexval);
+            int decValue = int.Parse(hexval, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine("Convert to-");
+            Console.WriteLine("Decimal number: " + decValue);
+        }
+    }
+}
